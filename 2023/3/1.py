317db4d3e9bd2087c5f0714f59d14fbd495442f9
Paul Keller <contact.keller.paul@gmail.com>
2023-12-06 19:16:29 +0100

    ignore of old files

--- 2023/3/1.py
+++ 2023/3/1.py
@@ -1,7 +1,7 @@
 import os, sys
 
 
-with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+with open(os.path.join(sys.path[0],"reddit.txt"), "r", encoding="utf-8") as f:
     text = f.read().strip()
     lines = text.split("\n")
     result = 0
