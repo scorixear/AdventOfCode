b055fc6e3c380c5eacf91f82c7cfa3ee75fa3e2a
Paul Keller <contact.keller.paul@gmail.com>
2023-12-09 11:33:19 +0100

    day 9

--- 2023/9/1.py
+++ 2023/9/1.py
@@ -1,10 +1,30 @@
 import os, sys
 import time
 
+def is_zero_sequence(sequence: list[int]) -> bool:
+    return all(x == 0 for x in sequence)
+
+def extrapolate(line: str) -> int:
+    sequences = [[int(x) for x in line.split()]]
+    while (not is_zero_sequence(sequences[-1])):
+        sequences.append([sequences[-1][i+1] - sequences[-1][i] for i in range(len(sequences[-1])-1)])
+    # print(sequences)
+    sequences[-1].append(0)
+    for i in range(len(sequences)-2, -1, -1):
+        sequences[i].append(sequences[i][-1] + sequences[i+1][-1])
+    return sequences[0][-1]
+    
+    
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    result = 0
+    for line in lines:
+        result += extrapolate(line)
+    print(result)
+    
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2023/9/2.py
+++ 2023/9/2.py
@@ -1,10 +1,29 @@
 import os, sys
 import time
 
+def is_zero_sequence(sequence: list[int]) -> bool:
+    return all(x == 0 for x in sequence)
+
+def extrapolate(line: str) -> int:
+    sequences = [[int(x) for x in line.split()]]
+    while (not is_zero_sequence(sequences[-1])):
+        sequences.append([sequences[-1][i+1] - sequences[-1][i] for i in range(len(sequences[-1])-1)])
+    sequences[-1].insert(0,0)
+    for i in range(len(sequences)-2, -1, -1):
+        sequences[i].insert(0, sequences[i][0] - sequences[i+1][0])
+    return sequences[0][0]
+    
+    
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    result = 0
+    for line in lines:
+        result += extrapolate(line)
+    print(result)
+    
 
 if __name__ == "__main__":
     before = time.perf_counter()
