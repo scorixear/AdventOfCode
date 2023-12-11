98328afe0a8f990f199952c89db00f5286086353
Paul Keller <contact.keller.paul@gmail.com>
2023-12-11 11:29:21 +0100

    day 11

--- 2023/11/1.py
+++ 2023/11/1.py
@@ -6,6 +6,42 @@ def main():
         text = f.read().strip()
         lines = text.split("\n")
 
+    # initialize empty columns and rows
+    empty_rows = set(list(range(len(lines))))
+    empty_columns = set(list(range(len(lines[0]))))
+    
+    # remove non-empty columns
+    for y, row in enumerate(lines):
+        # for each cell in that row
+        for x, cell in enumerate(row):
+            # if the cell is not empty, remove the column from the empty columns
+            if cell == "#":
+                empty_columns.discard(x)
+                empty_rows.discard(y)
+    
+    # find all galaxies
+    galaxies: list[tuple[int, int]] = []
+    expansion_size = 2
+    current_additional_y = 0
+    for y, row in enumerate(lines):
+        if y in empty_rows:
+            current_additional_y += 1
+            continue
+        current_additional_x = 0
+        for x, cell in enumerate(row):
+            if x in empty_columns:
+                current_additional_x += 1
+                continue
+            if cell == "#":
+                galaxies.append((x + current_additional_x * (expansion_size-1) , y + current_additional_y * (expansion_size-1)))
+    
+    total_distance = 0
+    for i in range(len(galaxies)-1):
+        for j in range(i+1, len(galaxies)):
+            x, y = galaxies[i]
+            x2, y2 = galaxies[j]
+            total_distance += abs(x2-x) + abs(y2-y)
+    print(total_distance)
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
