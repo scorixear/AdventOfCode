7c4f139e4b0b5f744e2317a9344b168c97637676
Paul Keller <contact.keller.paul@gmail.com>
2023-12-19 13:39:41 +0100

    day 19

--- 2023/19/1.py
+++ 2023/19/1.py
@@ -1,10 +1,104 @@
 import os, sys
 import time
 
+class Item:
+    def __init__(self, x: int, m: int, a: int, s: int):
+        self.x = x
+        self.m = m
+        self.a = a
+        self.s = s
+
+class Workflow:
+    def __init__(self, steps: list[tuple[str, bool, int, str]]):
+        self.steps: list[tuple[str, bool, int, str]] = steps
+    
+    def check(self, item: Item):
+        is_rejected = False
+        next_workflow = None
+        for (value, operation, threshold, target) in self.steps:
+            if threshold == None:
+                if target == "R":
+                    is_rejected = True
+                    break
+                if target == "A":
+                    break
+                next_workflow = target
+                break
+            item_value = getattr(item, value)
+            if operation:
+                if item_value > threshold:
+                    if target == "R":
+                        is_rejected = True
+                        break
+                    if target == "A":
+                        break
+                    next_workflow = target
+                    break
+            else:
+                if item_value < threshold:
+                    if target == "R":
+                        is_rejected = True
+                        break
+                    if target == "A":
+                        break
+                    next_workflow = target
+                    break
+        return (next_workflow, is_rejected)
+
+
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    workflows: dict[str, Workflow] = {}
+    items: list[tuple(Item, str)] = []
+    init_workflow = True
+    for line in lines:
+        if line == "":
+            init_workflow = False
+            continue
+        if init_workflow:
+            name, steps = line.split("{", 1)
+            steps = steps[:-1].split(",")
+            workflow_steps = []
+            for step in steps:
+                parts = step.split(":")
+                if len(parts) == 1:
+                    workflow_steps.append((None, None, None, parts[0]))
+                else:
+                    target = parts[1]
+                    if ">" in parts[0]:
+                        value, threshold = parts[0].split(">")
+                        operation = True
+                    else:
+                        value, threshold = parts[0].split("<")
+                        operation = False
+                    threshold = int(threshold)
+                    workflow_steps.append((value, operation, threshold, target))
+            workflows[name] = Workflow(workflow_steps)
+        else:
+            values = line[1:-1].split(",")
+            int_values = []
+            for value in values:
+                int_values.append(int(value.split("=")[1]))
+            items.append((Item(int_values[0], int_values[1], int_values[2], int_values[3]), "in"))
+            
+    accepted_items = []
+    while items:
+        item, workflow_name = items.pop()
+        new_workflow, is_rejected = workflows[workflow_name].check(item)
+        if new_workflow:
+            items.append((item, new_workflow))
+        elif not is_rejected:
+            accepted_items.append(item)
+    total = 0
+    for item in accepted_items:
+        total += item.x+item.m+item.a+item.s
+    print(total)
+        
+            
+                    
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2023/19/2.py
+++ 2023/19/2.py
@@ -1,10 +1,115 @@
+from collections import deque
 import os, sys
 import time
+
+class Workflow:
+    def __init__(self, steps: list[tuple[str, bool, int, str]]):
+        self.rules: list[tuple[str, bool, int, str]] = steps
+    
+    def apply_value(self, operation: str, threshold: int, low: int, high: int):
+        if operation == ">":
+            low = max(low, threshold+1)
+        elif operation == "<":
+            high = min(high, threshold)
+        elif operation == ">=":
+            low = max(low, threshold)
+        else:
+            high = min(high, threshold+1)
+        return low, high
+    
+    def check(self, x_l: int, x_h: int, m_l: int, m_h: int, a_l: int, a_h: int, s_l: int, s_h: int, workflow_queue: deque[tuple[str, int, int, int, int, int, int, int, int]]):
+        # for each rule
+        for value, operation, threshold, target in self.rules:
+            # if no threshold is given
+            if threshold == None:
+                # add to the queue
+                workflow_queue.appendleft((target, x_l, x_h, m_l, m_h, a_l, a_h, s_l, s_h))
+                return
+            op = ">" if operation else "<"
+            # depending on each value of the rule
+            if value == "x":
+                # the region that satisfies the rule
+                new_x_l, new_x_h = self.apply_value(op, threshold, x_l, x_h)
+                # will be appended to the queue
+                workflow_queue.append((target, new_x_l, new_x_h, m_l, m_h, a_l, a_h, s_l, s_h))
+                # the region that does not satisfy the rule
+                # will be checked in the next iteration
+                x_l, x_h = self.apply_value("<=" if operation else ">=", threshold, x_l, x_h)
+            elif value == "m":
+                new_m_l, new_m_h = self.apply_value(op, threshold, m_l, m_h)
+                workflow_queue.append((target, x_l, x_h, new_m_l, new_m_h, a_l, a_h, s_l, s_h))
+                m_l, m_h = self.apply_value("<=" if operation else ">=", threshold, m_l, m_h)
+            elif value == "a":
+                new_a_l, new_a_h = self.apply_value(op, threshold, a_l, a_h)
+                workflow_queue.append((target, x_l, x_h, m_l, m_h, new_a_l, new_a_h, s_l, s_h))
+                a_l, a_h = self.apply_value("<=" if operation else ">=", threshold, a_l, a_h)
+            else:
+                new_s_l, new_s_h = self.apply_value(op, threshold, s_l, s_h)
+                workflow_queue.append((target, x_l, x_h, m_l, m_h, a_l, a_h, new_s_l, new_s_h))
+                s_l, s_h = self.apply_value("<=" if operation else ">=", threshold, s_l, s_h) 
 
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    workflows: dict[str, Workflow] = {}
+    # workflow parsing
+    for line in lines:
+        # ignore items
+        if line == "":
+            break
+        # split workflow name and rules
+        name, steps = line.split("{", 1)
+        # split rules by comma
+        steps = steps[:-1].split(",")
+        workflow_steps = []
+        # for each rule
+        for step in steps:
+            # split rule and target
+            parts = step.split(":")
+            # if no rule is given
+            if len(parts) == 1:
+                # add default rule
+                workflow_steps.append((None, None, None, parts[0]))
+            # else add rule
+            else:
+                target = parts[1]
+                # if rule is > comparison
+                if ">" in parts[0]:
+                    value, threshold = parts[0].split(">")
+                    operation = True
+                else:
+                    value, threshold = parts[0].split("<")
+                    operation = False
+                threshold = int(threshold)
+                workflow_steps.append((value, operation, threshold, target))
+        workflows[name] = Workflow(workflow_steps)
+    
+    # contains workflows to be processed and their respective ranges
+    workflow_queue: deque[tuple[str, int, int, int, int, int, int]] = deque([(
+        "in", 1, 4001, 1, 4001, 1, 4001, 1, 4001)])
+    total = 0
+    # while we have more workflows to process
+    while workflow_queue:
+        # pop workflow and ranges
+        workflow, x_l, x_h, m_l, m_h, a_l, a_h, s_l, s_h = workflow_queue.pop()
+        # if the ranges are invalid, ignore
+        if x_l > x_h or m_l > m_h or a_l > a_h or s_l > s_h:
+            continue
+        # if the workflow is accepted, add to total
+        if workflow == "A":
+            total += (x_h-x_l)*(m_h-m_l)*(a_h-a_l)*(s_h-s_l)
+            continue
+        # if the workflow is rejected, ignore
+        if workflow == "R":
+            continue
+        workflow = workflows[workflow]
+        # perform workflow
+        workflow.check(x_l, x_h, m_l, m_h, a_l, a_h, s_l, s_h, workflow_queue)
+    print(total)
+
+        
+                    
 
 if __name__ == "__main__":
     before = time.perf_counter()
