e9b5f10f6266967af6f5bbcd87907171cc8c306a
Paul Keller <contact.keller.paul@gmail.com>
2023-12-21 12:06:25 +0100

    day 21

--- 2023/21/1.py
+++ 2023/21/1.py
@@ -1,11 +1,46 @@
 import os, sys
 import time
 
+def get_start(grid: list[str]):
+    for y,line in enumerate(grid):
+        for x,c in enumerate(line):
+            if c == "S":
+                return (x,y)
+
+def bfs(grid: list[str], start: tuple[int, int], max_steps: int):
+    modulo_remainder = max_steps % 2
+    reached = 0
+    seen = set()
+    queue = [(start, 0)]
+    while queue:
+        pos, steps = queue.pop(0)
+        if pos in seen:
+            continue
+        seen.add(pos)
+        x,y = pos
+        if grid[y][x] == "#":
+            continue
+        if steps % 2 == modulo_remainder:
+            reached += 1
+        if steps >= max_steps:
+            continue
+       
+        queue.append(((x+1,y), steps+1))
+        queue.append(((x-1,y), steps+1))
+        queue.append(((x,y+1), steps+1))
+        queue.append(((x,y-1), steps+1))
+    return reached
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-
+    starting_pos = get_start(lines)
+    print(bfs(lines, starting_pos, 64))
+    
+    
+        
+                
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
--- 2023/21/2.py
+++ 2023/21/2.py
@@ -1,11 +1,74 @@
 import os, sys
 import time
+def get_start(grid: list[list[str]]):
+    for y,line in enumerate(grid):
+        for x,c in enumerate(line):
+            if c == "S":
+                grid[y][x] = "."
+                return (x,y)
+
+def bfs(grid: list[list[str]], start: tuple[int, int], max_steps: int):
+    # quadratic formula, three factors a, b, c for y = a + bx + cx^2
+    factors = [0, 0, 0]
+    # index of factor to set
+    factor_index = 0
+    # set of positions to check
+    positions = {start}
+    # directions to travel to
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
+    # size of grid (is square)
+    n = len(grid)
+    
+    target = n//2
+    
+    # take first 1000 steps to get three distinct points on the quadratic curve
+    for step in range(1, 1000):
+        new_positions = set()
+        # for each position to be observed
+        for x,y in positions:
+            # take step in each direction
+            for dx,dy in directions:
+                new_x = x + dx
+                new_y = y + dy
+                # if the new position is a garden
+                if grid[new_y%n][new_x%n] != "#":
+                    # add it to the set of positions to be observed
+                    new_positions.add((new_x, new_y))
+        positions = new_positions
+        # if we reached a new repetition, which is n steps away, starting from n//2 (full grid added)
+        if step == target + n*factor_index:
+            # get the number of positions reached
+            factors[factor_index] = len(positions)
+            # increment the factor index
+            factor_index += 1
+            # if we have all three factors, we can calculate the quadratic formula
+            if factor_index == 3:
+                # the factors are actually the differences between the number of reached positions
+                delta0, delta1, delta2 = factors[0], factors[1]-factors[0], factors[2]-2*factors[1]+factors[0]
+                # x represents the number of grids added
+                # therefore x = max_steps//n
+                return delta0 + delta1*(max_steps//n) + delta2 * ((max_steps//n) * ((max_steps//n) - 1) // 2)
+    # if we didn't reach the quadratic formula, we can't calculate the number of reached positions
+    return -1
 
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-
+    grid = [[c for c in line] for line in lines]
+    
+    starting_pos = get_start(grid)
+    steps = 26501365
+    # n = len(lines)
+    # print(starting_pos)
+    # print(steps // n, steps % n)
+    # steps = 202300 * 131 + 65
+    print(bfs(grid, starting_pos, steps))
+    #print(bfs(lines, starting_pos, 5000))
+    
+    
+        
+                
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
