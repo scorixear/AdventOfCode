1f423c62cdb5c515f040c4881ad3846b73111314
Paul Keller <paul.keller@meierhofer.com>
2024-12-04 13:17:43 +0100

    day 4

--- 2024/4/1.py
+++ 2024/4/1.py
@@ -1,3 +1,4 @@
+from re import search
 import os, sys
 import time
 
@@ -5,7 +6,36 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-
+    search_key = "XMAS"
+    width = len(lines[0])
+    height = len(lines)
+    search_len = len(search_key)
+    # horizontal search
+    counter = 0
+    for line in lines:
+        for i in range(width - search_len + 1):
+            word = line[i:i+search_len]
+            if word == search_key or word == search_key[::-1]:
+                counter+=1 
+    # vertical search
+    for i in range(width):
+        for j in range(height - search_len + 1):
+            word = "".join([lines[j+k][i] for k in range(search_len)])
+            if word == search_key or word == search_key[::-1]:
+                counter+=1
+    # diagonal right search
+    for i in range(width - search_len + 1):
+        for j in range(height - search_len + 1):
+            word = "".join([lines[j+k][i+k] for k in range(search_len)])
+            if word == search_key or word == search_key[::-1]:
+                counter+=1
+    # diagonal left search
+    for i in range(search_len - 1, width):
+        for j in range(height - search_len + 1):
+            word = "".join([lines[j+k][i-k] for k in range(search_len)])
+            if word == search_key or word == search_key[::-1]:
+                counter+=1
+    print(counter)
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
--- 2024/4/2.py
+++ 2024/4/2.py
@@ -5,6 +5,18 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    width = len(lines[0])
+    height = len(lines)
+    search_key = "MAS"
+    search_len = len(search_key)
+    counter = 0
+    for i in range(width - search_len + 1):
+        for j in range(height - search_len + 1):
+            diag_right = "".join([lines[j+k][i+k] for k in range(search_len)])
+            diag_left = "".join([lines[j+k][i+search_len-1-k] for k in range(search_len)])
+            if (diag_right == search_key or diag_right == search_key[::-1]) and (diag_left == search_key or diag_left == search_key[::-1]):
+                counter+=1
+    print(counter)
 
 if __name__ == "__main__":
     before = time.perf_counter()
