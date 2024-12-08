2bc2bb4017a1f5ccb071e90849791e634362b227
Paul Keller <contact.keller.paul@gmail.com>
2024-12-08 14:14:15 +0100

    day 8

--- 2024/8/1.py
+++ 2024/8/1.py
@@ -5,6 +5,34 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    plane = [list(line) for line in lines]
+    width = len(plane[0])
+    height = len(plane)
+    
+    antennas: dict[str, list[tuple[int, int]]] = {}
+    for y in range(height):
+        for x in range(width):
+            if plane[y][x] != ".":
+                if plane[y][x] in antennas:
+                    antennas[plane[y][x]].append((x, y))
+                else:
+                    antennas[plane[y][x]] = [(x, y)]
+    found: set[tuple[int, int]] = set()
+    for ant in antennas.values():
+        for i in range(0, len(ant)-1):
+            for j in range(i+1, len(ant)):
+                new_antinodes = find_antinodes(ant[i], ant[j])
+                for antinode in new_antinodes:
+                    if 0 <= antinode[0] < width and 0 <= antinode[1] < height:
+                        found.add(antinode)
+    print(len(found))
+
+def find_antinodes(ant1: tuple[int, int], ant2: tuple[int, int]) -> list[tuple[int, int]]:
+    dist = (ant1[0] - ant2[0], ant1[1] - ant2[1])
+    return [
+        (ant1[0] + dist[0], ant1[1] + dist[1]),
+        (ant2[0] - dist[0], ant2[1] - dist[1])
+    ]
 
 if __name__ == "__main__":
     before = time.perf_counter()
