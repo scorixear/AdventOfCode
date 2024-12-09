75a27f18d6c9ff7733a6d2e0da94cd24808e9c55
Paul Keller <paul.keller@meierhofer.com>
2024-12-09 08:59:25 +0100

    day 9

--- 2024/9/1.py
+++ 2024/9/1.py
@@ -4,7 +4,49 @@ import time
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
-        lines = text.split("\n")
+    disk: list[int | None] = []
+    mode = 0
+    idx = 0
+    for char in text:
+        # disk
+        if mode == 0:
+            length = int(char)
+            for _ in range(length):
+                disk.append(idx)
+            idx += 1
+        # free space
+        else:
+            length = int(char)
+            for _ in range(length):
+                disk.append(None)
+        mode ^= 1
+    # print(disk)
+
+    left = 0
+    right = len(disk) - 1
+    while True:
+        # find the first free space
+        for i in range(left, right + 1):
+            if disk[i] is None:
+                left = i
+                break
+        # find first disk
+        for i in range(right, left - 1, -1):
+            if disk[i] is not None:
+                right = i
+                break
+        disk[right], disk[left] = disk[left], disk[right]
+        left += 1
+        right -= 1
+        if left >= right:
+            break
+    # print(disk)
+    total = 0
+    for i, idx in enumerate(disk):
+        if idx is None:
+            break
+        total += i * idx
+    print(total)
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2024/9/2.py
+++ 2024/9/2.py
@@ -1,10 +1,76 @@
 import os, sys
 import time
 
+class File:
+    def __init__(self, size: int, idx: int, is_disk: bool):
+        self.size = size
+        self.is_disk = is_disk
+        self.idx = idx
+    def __str__(self):
+        if self.is_disk:
+            return f"{self.idx}" * self.size
+            # for _ in range(self.size):
+            #     result += str(self.idx)
+            # return result
+        else:
+            return "." * self.size
+    def __repr__(self) -> str:
+        return self.__str__()
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
-        lines = text.split("\n")
+    disk: list[File] = []
+    mode = 0
+    idx = 0
+    for char in text:
+        # disk
+        if mode == 0:
+            length = int(char)
+            disk.append(File(length, idx, True))
+            idx += 1
+        # free space
+        else:
+            length = int(char)
+            disk.append(File(length, -1, False))
+        mode ^= 1
+    #print(disk)
+
+    left = 0
+    right = len(disk) - 1
+    while True:
+        # find first disk
+        for i in range(right, left - 1, -1):
+            if disk[i].is_disk is True:
+                right = i
+                break
+        
+        # search left until space fits
+        for i in range(0, right + 1,):
+            if disk[i].is_disk is False and disk[i].size >= disk[right].size:
+                disk_to_move = disk[right]
+                disk[right] = File(disk[right].size, -1, False)
+                left_over_space = disk[i].size - disk_to_move.size
+                disk[i] = disk_to_move
+                if (left_over_space) > 0:
+                    disk.insert(i + 1, File(left_over_space, -1, False))
+                    right += 1
+                #print(disk)
+                break
+        right -= 1
+        if right <= 0:
+            break
+    #print(disk)
+    total = 0
+    current_file = 0
+    for i, idx in enumerate(disk):
+        if idx.is_disk is False:
+            current_file += idx.size
+            continue
+        for _ in range(idx.size):
+            total += current_file * idx.idx
+            current_file += 1
+    print(total)
 
 if __name__ == "__main__":
     before = time.perf_counter()
