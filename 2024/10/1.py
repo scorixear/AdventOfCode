749820c21d162324b526849d553ccfa98ced0e29
Paul Keller <paul.keller@meierhofer.com>
2024-12-10 11:49:40 +0100

    day 10

--- 2024/10/1.py
+++ 2024/10/1.py
@@ -2,10 +2,41 @@ import os, sys
 import time
 
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0],"example.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    grid = [[-1 if char == "." else int(char) for char in line] for line in lines]
 
+    start_pos: list[tuple[int, int]] = []
+    width = len(grid[0])
+    height = len(grid)
+    for y in range(height):
+        for x in range(width):
+            if grid[y][x] == 0:
+                start_pos.append((x, y))
+    total = 0
+    for x, y in start_pos:
+        total += dfs(x, y, grid, width, height)
+    print(total)
+def dfs(x: int, y: int, grid: list[list[int]], width: int, height: int):
+    visited = [[False for _ in range(width)] for _ in range(height)]
+    visited[y][x] = True
+    stack = [(x, y)]
+    goals = 0
+    while stack:
+        x, y = stack.pop()
+        val = grid[y][x]
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            new_x, new_y = x + dx, y + dy
+            if new_x < 0 or new_x >= width or new_y < 0 or new_y >= height or visited[new_y][new_x] or grid[new_y][new_x] - 1 != val:
+                continue
+            visited[new_y][new_x] = True
+            
+            if grid[new_y][new_x] == 9:
+                goals += 1
+            else:
+                stack.append((new_x, new_y))
+    return goals
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
--- 2024/10/2.py
+++ 2024/10/2.py
@@ -5,7 +5,43 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    grid = [[-1 if char == "." else int(char) for char in line] for line in lines]
 
+    start_pos: list[tuple[int, int]] = []
+    width = len(grid[0])
+    height = len(grid)
+    for y in range(height):
+        for x in range(width):
+            if grid[y][x] == 0:
+                start_pos.append((x, y))
+    total = 0
+    for x, y in start_pos:
+        total += dfs(x, y, grid, width, height)
+    print(total)
+def dfs(x: int, y: int, grid: list[list[int]], width: int, height: int):
+    #visited = [[False for _ in range(width)] for _ in range(height)]
+    hiking_paths = [[0 for _ in range(width)] for _ in range(height)]
+    #visited[y][x] = True
+    hiking_paths[y][x] = 1
+    stack = [(x, y)]
+    reached_goals = set()
+    while stack:
+        x, y = stack.pop()
+        val = grid[y][x]
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            new_x, new_y = x + dx, y + dy
+            if new_x < 0 or new_x >= width or new_y < 0 or new_y >= height or grid[new_y][new_x] - 1 != val:
+                continue
+            # visited[new_y][new_x] = True
+            hiking_paths[new_y][new_x] += 1
+            if grid[new_y][new_x] == 9:
+                reached_goals.add((new_x, new_y))
+            else:
+                stack.append((new_x, new_y))
+    goals = 0
+    for x, y in reached_goals:
+        goals += hiking_paths[y][x]
+    return goals
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
