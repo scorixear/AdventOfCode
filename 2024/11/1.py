c2120aa7529254891c6941885dabe18143dbfc2f
Paul Keller <paul.keller@meierhofer.com>
2024-12-11 10:33:56 +0100

    day 11

--- 2024/11/1.py
+++ 2024/11/1.py
@@ -4,7 +4,20 @@ import time
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
-        lines = text.split("\n")
+    stones = list(map(int, text.split(" ")))
+    for _ in range(25):
+        for i in range(len(stones)-1, -1, -1):
+            if stones[i] == 0:
+                stones[i] = 1
+            elif len(str(stones[i])) % 2 == 0:
+                str_stone = str(stones[i])
+                stones[i] = int(str_stone[:len(str_stone) // 2])
+                stones.insert(i + 1, int(str_stone[len(str_stone) // 2:]))
+            else:
+                stones[i] *= 2024
+    # print(stones)    
+    print(len(stones))
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2024/11/2.py
+++ 2024/11/2.py
@@ -1,10 +1,41 @@
+from collections import defaultdict
 import os, sys
 import time
 
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
-        lines = text.split("\n")
+    stones = list(map(int, text.split(" ")))
+    stone_counter = dict()
+    for stone in stones:
+        if stone not in stone_counter:
+            stone_counter[stone] = 1
+        else:
+            stone_counter[stone] += 1
+    for _ in range(75):
+        new_stones = dict()
+        for stone, count in stone_counter.items():
+            if stone == 0:
+                set_or_increment(new_stones, 1, count)
+            else:
+                str_stone = str(stone)
+                if len(str_stone) % 2 == 0:
+                    set_or_increment(new_stones, int(str_stone[:len(str_stone) // 2]), count)
+                    set_or_increment(new_stones, int(str_stone[len(str_stone) // 2:]), count)
+                else:
+                    set_or_increment(new_stones, stone * 2024, count)
+        stone_counter = new_stones
+    # print(stones)  
+    total = 0
+    for stone, count in stone_counter.items():
+        total += count 
+    print(total)
+
+def set_or_increment(dictionary, key, count):
+    if key not in dictionary:
+        dictionary[key] = count
+    else:
+        dictionary[key] += count
 
 if __name__ == "__main__":
     before = time.perf_counter()
