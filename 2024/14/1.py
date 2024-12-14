272885564c7e07dfb39df908de170809ae85f153
Paul Keller <paul.keller@meierhofer.com>
2024-12-14 09:37:09 +0100

    day 14

--- 2024/14/1.py
+++ 2024/14/1.py
@@ -1,10 +1,55 @@
 import os, sys
 import time
 
+class Robot:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+    def step(self, steps: int, width: int, height: int):
+        for _ in range(steps):
+            self.x += self.vx
+            self.y += self.vy
+            if self.x < 0:
+                self.x += width
+            elif self.x >= width:
+                self.x -= width
+            if self.y < 0:
+                self.y += height
+            elif self.y >= height:
+                self.y -= height
+        
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+
+    seconds = 100
+    width = 101 #11
+    height = 103 #7
+    robots = []
+    quadrants = [0, 0, 0, 0]
+    for line in lines:
+        p, v = line.split(" ")
+        x, y = map(int, p[2:].split(","))
+        vx, vy = map(int, v[2:].split(","))
+        robot = Robot(x, y, vx, vy)
+        robot.step(seconds, width, height)
+        robots.append(robot)
+        if robot.x < width // 2:
+            if robot.y < height // 2:
+                quadrants[0] += 1
+            elif robot.y > height // 2:
+                quadrants[1] += 1
+        elif robot.x > width // 2:
+            if robot.y < height // 2:
+                quadrants[2] += 1
+            elif robot.y > height // 2:
+                quadrants[3] += 1
+    print(quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3])
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2024/14/2.py
+++ 2024/14/2.py
@@ -1,10 +1,68 @@
 import os, sys
 import time
 
+class Robot:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+    def step(self, width: int, height: int):
+        self.x += self.vx
+        self.y += self.vy
+        if self.x < 0:
+            self.x += width
+        elif self.x >= width:
+            self.x -= width
+        if self.y < 0:
+            self.y += height
+        elif self.y >= height:
+            self.y -= height
+           
+        
+
 def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+
+    seconds = 100
+    width = 101 #11
+    height = 103 #7
+    robots = []
+    for line in lines:
+        p, v = line.split(" ")
+        x, y = map(int, p[2:].split(","))
+        vx, vy = map(int, v[2:].split(","))
+        robot = Robot(x, y, vx, vy)
+        robots.append(robot)
+    seconds_counter = 1
+    max_neighbours = 0
+    while True:
+        robot_positions: set[tuple[int, int]] = set()
+        for robot in robots:
+            robot.step(width, height)
+            robot_positions.add((robot.x, robot.y))
+        
+        neighbour_count = 0
+        for x, y in robot_positions:
+            for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                new_x, new_y = x + dx, y + dy
+                if (new_x, new_y) in robot_positions:
+                    neighbour_count += 1
+                    break
+        if neighbour_count > max_neighbours:
+            input("Press Enter to continue...")
+            max_neighbours = neighbour_count
+            grid = [[" " for _ in range(width)] for _ in range(height)]
+            for robot in robots:
+                grid[robot.y][robot.x] = "#"
+            for row in grid:
+                print("".join(row))
+            print(seconds_counter)
+        seconds_counter += 1
+
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
