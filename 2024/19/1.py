9c18f2679c31f618902d001167ed9134cd5312cd
Paul Keller <contact.keller.paul@gmail.com>
2024-12-19 19:20:36 +0100

    day 19

--- 2024/19/1.py
+++ 2024/19/1.py
@@ -5,6 +5,24 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    stripes = lines[0].split(", ")
+    patterns = lines[2:]
+    
+    counter = 0
+    for pattern in patterns:
+        if find_path(stripes, pattern):
+            counter += 1
+    print(counter)
+    
+def find_path(stripes: list[str], pattern: str) -> bool:
+    if len(pattern) == 0:
+        return True
+    for stripe in stripes:
+        if pattern.startswith(stripe):
+            if find_path(stripes, pattern[len(stripe):]):
+                return True
+    return False
+    
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2024/19/2.py
+++ 2024/19/2.py
@@ -5,6 +5,27 @@ def main():
     with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    stripes = lines[0].split(", ")
+    patterns = lines[2:]
+    
+    counter = 0
+    dp: dict[str, int] = {}
+    for pattern in patterns:
+        counter += find_path(stripes, pattern, dp)
+    print(counter)
+    
+def find_path(stripes: list[str], pattern: str, dp: dict[str, int]) -> int:
+    if len(pattern) == 0:
+        return 1
+    if pattern in dp:
+        return dp[pattern]
+    total = 0
+    for stripe in stripes:
+        if pattern.startswith(stripe):
+            total += find_path(stripes, pattern[len(stripe):], dp)
+    dp[pattern] = total
+    return total
+    
 
 if __name__ == "__main__":
     before = time.perf_counter()
