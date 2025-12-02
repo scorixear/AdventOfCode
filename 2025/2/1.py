d907ea9159b702edc4a47b6539df0a35c3fe974d
Paul Keller <contact.keller.paul@gmail.com>
2025-12-02 18:18:37 +0100

    2025 day 2

--- 2025/2/1.py
+++ 2025/2/1.py
@@ -1,10 +1,22 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    ranges = [tuple(map(int, r.split("-"))) for r in lines[0].split(",")]
+    total = 0
+    for a, b in ranges:
+        for n in range(a, b + 1):
+            strn = str(n)
+            if len(strn) % 2 == 0:
+                left, right = strn[: len(strn) // 2], strn[len(strn) // 2 :]
+                if left == right:
+                    total += n
+    print(total)
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2025/2/2.py
+++ 2025/2/2.py
@@ -1,10 +1,27 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    ranges = [tuple(map(int, r.split("-"))) for r in lines[0].split(",")]
+    total = 0
+    for a, b in ranges:
+        for n in range(a, b + 1):
+            strn = str(n)
+            for mod in range(2, len(strn) + 1):
+                if len(strn) % mod == 0:
+                    part_len = len(strn) // mod
+                    parts = [
+                        strn[i * part_len : (i + 1) * part_len] for i in range(mod)
+                    ]
+                    if all(p == parts[0] for p in parts):
+                        total += n
+                        break
+    print(total)
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
