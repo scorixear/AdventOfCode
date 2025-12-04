a7cfee4b27dbb098b6f3f3f0532c88295f078712
Paul Keller <contact.keller.paul@gmail.com>
2025-12-04 17:36:30 +0100

    2025 day 4

--- 2025/4/1.py
+++ 2025/4/1.py
@@ -1,10 +1,30 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    rolls = set()
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "@":
+                rolls.add((x, y))
+
+    total = 0
+    direction = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+    for roll in rolls:
+        x, y = roll
+        roll_count = 0
+        for dx, dy in direction:
+            nx, ny = x + dx, y + dy
+            if (nx, ny) in rolls:
+                roll_count += 1
+        if roll_count < 4:
+            total += 1
+    print(total)
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2025/4/2.py
+++ 2025/4/2.py
@@ -1,10 +1,46 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    rolls = set()
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "@":
+                rolls.add((x, y))
+
+    was_removed = True
+    total = 0
+    direction = [
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, 1),
+        (0, -1),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+    ]
+    while was_removed:
+        to_be_removed = set()
+        for roll in rolls:
+            x, y = roll
+            roll_count = 0
+            for dx, dy in direction:
+                nx, ny = x + dx, y + dy
+                if (nx, ny) in rolls:
+                    roll_count += 1
+            if roll_count < 4:
+                total += 1
+                to_be_removed.add(roll)
+        rolls -= to_be_removed
+        was_removed = len(to_be_removed) > 0
+
+    print(total)
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
