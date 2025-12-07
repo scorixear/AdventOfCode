254b8591e07c2398ba6e0a0e88228f62b92d8ced
Paul Keller <contact.keller.paul@gmail.com>
2025-12-07 09:37:35 +0100

    2025 day 7

--- 2025/7/1.py
+++ 2025/7/1.py
@@ -1,10 +1,42 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    splitter = []
+    start = (0, 0)
+    width = len(lines[0])
+    height = len(lines)
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "^":
+                splitter.append((x, y))
+            elif c == "S":
+                start = (x, y)
+
+    queue = [start]
+    visited = set()
+    hit_spliters = 0
+    while queue:
+        x, y = queue.pop(0)
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        if y + 1 >= height:
+            continue
+        if (x, y + 1) in splitter:
+            hit_spliters += 1
+            if x - 1 >= 0:
+                queue.append((x - 1, y + 1))
+            if x + 1 < width:
+                queue.append((x + 1, y + 1))
+        else:
+            queue.append((x, y + 1))
+    print(hit_spliters)
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2025/7/2.py
+++ 2025/7/2.py
@@ -1,10 +1,43 @@
 import os, sys
 import time
 
+
 def main():
-    with open(os.path.join(sys.path[0],"input.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0], "input.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    splitter = []
+    start = (0, 0)
+    width = len(lines[0])
+    height = len(lines)
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "^":
+                splitter.append((x, y))
+            elif c == "S":
+                start = (x, y)
+
+    dp = {}
+    result = dfs(start[0], start[1], height, width, splitter, dp)
+    print(result)
+
+
+def dfs(x, y, height, width, splitter, dp) -> int:
+    if (x, y) in dp:
+        return dp[(x, y)]
+    if y + 1 >= height:
+        return 1
+    total = 0
+    if (x, y + 1) in splitter:
+        if x - 1 >= 0:
+            total += dfs(x - 1, y + 1, height, width, splitter, dp)
+        if x + 1 < width:
+            total += dfs(x + 1, y + 1, height, width, splitter, dp)
+    else:
+        total += dfs(x, y + 1, height, width, splitter, dp)
+    dp[(x, y)] = total
+    return total
+
 
 if __name__ == "__main__":
     before = time.perf_counter()
